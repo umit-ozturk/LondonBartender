ebb83c0e2e069d10e577e205819cbd4bdb015993
Umit Ozturk <umit.ozturk.9716@gmail.com>
2021-10-08 17:23:31 +0300

    Home Page Improved

--- london_bartender/users/models.py
+++ london_bartender/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -8,7 +8,10 @@ class User(AbstractUser):
     """Default user for London Bartender."""
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    profile_photo = models.ImageField(
+        _("Profile Photo"), upload_to="images/users", null=True, blank=True
+    )
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
