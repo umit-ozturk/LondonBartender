deadf80b3ac661f36987f428f5ed1375997400c0
Umit Ozturk <umit.ozturk.9716@gmail.com>
2021-10-12 18:24:41 +0300

    Pagination for Home Page

--- london_bartender/drinks/views.py
+++ london_bartender/drinks/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from london_bartender.drinks.models import Cocktail
 
@@ -6,6 +6,11 @@ from london_bartender.drinks.models import Cocktail
 class ClassicCocktailsListView(ListView):
     model = Cocktail
     template_name = "drinks/cocktail_classic_list.html"
+    paginate_by = 4
 
     def get_queryset(self):
         return super(ClassicCocktailsListView, self).get_queryset().filter(classic=True)
+
+
+class CocktailDetailView(DetailView):
+    model = Cocktail
--- london_bartender/posts/views.py
+++ london_bartender/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 
 from london_bartender.posts.models import Post
@@ -8,6 +9,6 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data()
-        context["post_list"] = Post.objects.all()
+        context["post_list"] = Paginator(Post.objects.all(), 4).get_page("1")
         context["featured_post_list"] = Post.objects.filter(featured=True)[:3]
         return context
