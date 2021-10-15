384f04404ee64fe27bde00884163f6b871fe45b2
Umit Ozturk <umit.ozturk.9716@gmail.com>
2021-10-15 17:21:29 +0300

    Classic Cocktails Listed on Home Page

--- london_bartender/posts/views.py
+++ london_bartender/posts/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 
+from london_bartender.drinks.models import Cocktail
 from london_bartender.posts.models import Post
 
 
@@ -9,6 +10,8 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data()
-        context["post_list"] = Paginator(Post.objects.all(), 4).get_page("1")
+        context["classic_cocktail_list"] = Paginator(
+            Cocktail.objects.filter(classic=True), 4
+        ).get_page("1")
         context["featured_post_list"] = Post.objects.filter(featured=True)[:3]
         return context
